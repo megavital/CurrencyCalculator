24ef33cd870a60d1d67c6585a0ac55e28c4af3a0
MegaVital <kalist.fl@gmail.com>
2023-09-09 17:08:01 +0300

    reverse2

--- src/CurrencyCalculator.tsx
+++ src/CurrencyCalculator.tsx
@@ -85,8 +85,7 @@ export const CurrencyCalculator: FC<CurrencyCalculatorProps> = () => {
             }
           </select>
           <button className='reverse' onClick={() => { reverse(); setButtonTrue(true) }}><svg
-            width="24"
-            height="24"
+
             viewBox="0 0 24 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
